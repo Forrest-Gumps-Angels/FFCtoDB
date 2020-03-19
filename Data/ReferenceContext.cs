fc7cfcd9de00dae583708563d535d73f8e89845f
Rasmus <201806493@post.au.dk>
2020-03-19 10:34:27 +0100

    Add project files.

---
+++ Data/ReferenceContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using FFCtoDB.Models;
+
+namespace FFCtoDB.Data
+{
+    public class ReferenceContext : DbContext
+    {
+
+        private string _connectionString = "Source";
+        public DbSet<Reference> References { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder ob)
+        {
+            ob.UseSqlServer(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Reference>().HasKey(c => new { c.xPoint, c.yPoint });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using FFCtoDB.Models;
+using FFCtoDB.Data;
+
+namespace FFCtoDB
+{
+    public class Program
+    { 
+        static void Main(string[] args)
+        {
+            using (var db = new ReferenceContext())
+            {
+                db.Add(new Reference { xPoint = 0, yPoint = 0, RSSI = 0 });
+            }
+        }
+    }
+}
